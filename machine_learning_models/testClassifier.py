d50c6bba214532289f718f932f7f280b286f8ef9
Katherine Bancroft <katherinebancroft@Katherines-MacBook-Pro.local>
2019-03-17 09:28:54 -0400

    resolved conflict in routes

--- machine_learning_models/testClassifier.py
+++ machine_learning_models/testClassifier.py
@@ -34,7 +34,7 @@ def load_trained_model():
     HEIGHT = 300
     WIDTH = 300
     BATCH_SIZE = 32
-    class_list = ["avalanche", "flood", "forrest_fire"]
+    class_list = ["avalanche", "flood", "forest_fire"]
     FC_LAYERS = [1024, 1024]
     dropout = 0.5
     base_model = ResNet50(weights='imagenet', 
@@ -58,6 +58,15 @@ def model_predict(image_path, model, filename):
         return True
     return False
 
+def model_predict_image(image_path, model):
+    class_list = ["avalanche", "flood", "fire"]
+    im = cv2.imread(image_path)
+    resized_image = cv2.resize(im, (300, 300)) 
+    tr_img_data = np.expand_dims(resized_image, axis=0) 
+    prediction = model.predict(tr_img_data)
+    print(class_list[np.argmax(prediction)])
+    return class_list[np.argmax(prediction)]
+
 def test_model():
     model = load_trained_model()
     count = 0
@@ -70,6 +79,11 @@ def test_model():
         if model_predict(directory + filename, model, filename):
             count += 1
     print("Model accuracy is :" + str(count / filename_count))
+
+
+def run_model_on_one_image(url):
+    model = load_trained_model()
+    return model_predict(url, model, 'who_cares')
 
 if __name__ == "__main__":
     test_model()
