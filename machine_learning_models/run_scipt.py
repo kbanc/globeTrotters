081b103588daa391bc5a0c17c823c21b0b9c0e10
Katherine Bancroft <katherinebancroft@Katherines-MacBook-Pro.local>
2019-03-17 10:16:32 -0400

    script to connect ML models to DB

---
+++ machine_learning_models/run_scipt.py
@@ -0,0 +1,78 @@
+"""
+Katherine Bancroft
+
+Hack the Globe
+2019-03-17
+"""
+
+import requests
+from testClassifier import run_model_on_one_image
+
+class ProcessImages(object):
+
+    def _get_raw_data(self):
+        return requests.get(
+                'http://localhost:8888/reports/get_all_raw_image',
+            )
+        
+    
+    def _update_raw_data(self, message):
+        message['processed'] = True
+        requests.post(
+            'http://localhost:8888/reports/update_raw_data',
+            data=message
+        )
+
+    def process_and_send_image_data(self):  
+        raw_image_data = self._get_raw_data()
+        for image_content in raw_image_data.json():
+            self._update_raw_data(image_content)
+            disaster_type_prediction = run_model_on_one_image(image_content['image'])
+            processed_image = self._construct_good_message(image, disaster_type_prediction)
+            self._post_to_processed_db(processed_image)
+    
+    def _post_to_processed_db(self, message):
+        requests.post(
+            'http://localhost:8888/reports/create_good_report',
+            data=message
+        )
+    
+    def _construct_good_message(self, message, disaster_type_prediction):
+        message['disaster'] = disaster_type_prediction
+        return message
+
+class ProcessTweets(object):
+        
+    def _get_raw_data(self):
+        return requests.get(
+                'http://localhost:8888/reports/get_all_raw_tweet',
+            )
+    
+    def _update_raw_data(self, message):
+        message['processed'] = True
+        requests.post(
+            'http://localhost:8888/reports/update_raw_data',
+            data=message
+        )
+
+    def process_and_send_image_data(self):  
+        raw_tweet_data = self._get_raw_data()
+
+        for tweet in raw_tweet_data.json():
+            if type(tweet) == dict:
+                self._update_raw_data(tweet)
+                self._post_to_processed_db(tweet)
+    
+    def _post_to_processed_db(self, message):
+        requests.post(
+            'http://localhost:8888/reports/create_good_report',
+            data=message
+        )
+
+
+        
+image_processor = ProcessImages()
+image_processor.process_and_send_image_data()
+
+# tweet_processor = ProcessTweets()
+# tweet_processor.process_and_send_image_data()
