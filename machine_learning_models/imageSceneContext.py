687ae0c880b1b9b8220ccee1e73d3e97041ef66d
Shrey <shreysp23@hotmail.com>
2019-03-17 08:23:34 -0400

    Added a trained neural network based on transfer learning. Added Google API for scene context

--- machine_learning_models/imageSceneContext.py
+++ machine_learning_models/imageSceneContext.py
@@ -1,6 +1,6 @@
 import os
 import io
-
+import json
 #Imports google cloud client library
 #import google.cloud
 from google.cloud import vision
@@ -9,8 +9,11 @@ from google.cloud.vision import types
 # Instantiates a client
 client = vision.ImageAnnotatorClient()
 
-file_name = 'download.jpg'
-with io.open(file_name,'rb') as image_file:
+all_labels = {}
+
+filename = 'scene_context.png'
+# Loads the image into memory
+with io.open(filename, 'rb') as image_file:
     content = image_file.read()
 
 image = types.Image(content=content)
@@ -18,12 +21,10 @@ image = types.Image(content=content)
 # Performs label detection on the image file
 response = client.label_detection(image=image)
 labels = response.label_annotations
-
 for label in labels:
-    print(label.description)
+    label_type = label.description.strip()
+    all_labels[label_type] = label.score
 
-
-# os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="client_secrets.json"
-# client = vision.ImageAnnotatorClient()
-#if __name__ == '__main__':
- #   run_quickstart()
+s = [(k, all_labels[k]) for k in sorted(all_labels, key=all_labels.get, reverse=True)]
+with open('scene_context.txt', 'w') as file:
+     file.write(json.dumps(s))
