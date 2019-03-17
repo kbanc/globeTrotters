611afbf23e6ca5d9e14f0d89711cf551096f095e
Arash Nourimand <anourimand@gmail.com>
2019-03-16 21:21:02 -0400

    Made Imageprocess.py

--- imageprocess.py
+++ imageprocess.py
@@ -1,14 +1,23 @@
 import os
 import io
-import google.cloud
+
+#Imports google cloud client library
+#import google.cloud
 from google.cloud import vision
-vision_client = vision.Client()
+from google.cloud.vision import types
+
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+
 file_name = 'download.jpg'
 with io.open(file_name,'rb') as image_file:
     content = image_file.read()
-    image = vision_client.image(content=content)
 
-labels = image.detect_labels()
+image = types.Image(content=content)
+
+# Performs label detection on the image file
+response = client.label_detection(image=image)
+labels = response.label_annotations
 
 for label in labels:
     print(label.description)
@@ -16,3 +25,5 @@ for label in labels:
 
 # os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="client_secrets.json"
 # client = vision.ImageAnnotatorClient()
+#if __name__ == '__main__':
+ #   run_quickstart()
