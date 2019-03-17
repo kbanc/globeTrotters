805e6fa64c259d54f0af4a70ef54fefce565f7cc
Shrey <shreysp23@hotmail.com>
2019-03-17 10:24:35 -0400

    Modified algorithm to use corpus

--- machine_learning_models/imageSceneContext.py
+++ machine_learning_models/imageSceneContext.py
@@ -17,7 +17,8 @@ class ImageSceneContext:
 
         all_labels = {}
 
-        filename = 'severe.png'
+        filename = 'severe_2.png'
+        corpus_file_name = 'severe_2.txt'
         # Loads the image into memory
         with io.open(filename, 'rb') as image_file:
             content = image_file.read()
@@ -32,7 +33,7 @@ class ImageSceneContext:
             all_labels[label_type] = label.score
 
         s = [(k, all_labels[k]) for k in sorted(all_labels, key=all_labels.get, reverse=True)]
-        with open('not_severe.txt', 'w') as file:
+        with open(corpus_file_name, 'w') as file:
             file.write(json.dumps(s))
         
         # Ideally run the image through a classifier, but for now assume text file
@@ -44,23 +45,22 @@ class ImageSceneContext:
             s = open('corpus/flood_words.txt', 'r').read()
         else:
             return None
-
+        # Ideally run the image through a classifier, but for now assume text file
+        s = open('corpus/forest_fire_words.txt', 'r').read()
         top_words = eval(s)
-        s = open('not_severe.txt', 'r').read()
-        img = eval(s)
-        img = dict(img)
         top_words = dict(top_words)
-        try:
-            img["Fire"]
-        except:
-            img["Fire"] = 0
-        
         count_words = 0
         for key, value in top_words.items():
             count_words += value
         for key, value in top_words.items():
-            top_words[key] = top_words[key] / count_words * 100
-        return(str(top_words["Fire"]*img["Fire"]))
+            top_words[key] = top_words[key] / count_words
+        s = open(corpus_file_name, 'r').read()
+        img = eval(s)
+        img = dict(img)
+        score = 0
+        for key, value in img.items():
+            score += value * top_words[key]
+        return((1/score)*2)
 
 def main(): 
     scene_context = ImageSceneContext("forest_fire")
